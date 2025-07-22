9bdc3898682e855e675819297787fafa572d29a1
LemnsaLeger <lemnsadigitalservices@gmail.com>
2025-07-21 20:02:38 -0700

    HomePage Component Layout.

---
+++ my-app/src/components/HomePage.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+import GameBoard from "./GameBoard";
+
+function HomePage() {
+  const [level, setLevel] = useState("");
+
+  function handleEasy() {
+    setLevel("easy");
+  }
+
+  function handleMedium() {
+    setLevel("medium");
+  }
+
+  function handleHard() {
+    setLevel("hard");
+  }
+
+  if (level === "") {
+    return (
+      <>
+        <h1 className="header">Memory Card Game</h1>
+        <p>- play with your memory -</p>
+        <h2>Select Your Level</h2>{" "}
+        <section className="btns">
+          <button onClick={handleEasy}>Easy Peasy</button>{" "}
+          <button onClick={handleMedium}>medium</button>{" "}
+          <button onClick={handleHard}>Go Harder</button>{" "}
+        </section>{" "}
+      </>
+    );
+  } else if (level === "easy") {
+    return <GameBoard numberOfCards={3} />;
+  } else if (level === "medium") {
+    return <GameBoard numberOfCards={6} />
+  } else {
+    return <GameBoard numberOfCards={12} />
+  }
+}
+
+export default HomePage;

75ad5e3966e470eb417ea00b0b005dba572434d7
LemnsaLeger <lemnsadigitalservices@gmail.com>
2025-07-21 20:00:12 -0700

    Game Card Component with Props.

---
+++ my-app/src/components/Card.jsx
@@ -0,0 +1,12 @@
+function Card({imgUrl, label, onClick }) {
+    return (
+        <div className="card" onClick={onClick}>
+          <img srcSet={imgUrl} alt={label}/>
+          <p>{label}</p>
+          <p>index</p>
+        </div>
+    );
+}
+
+
+export default Card;

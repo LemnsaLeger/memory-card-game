f59d851a97b4438446769f2d20c3b234c8671f38
LemnsaLeger <lemnsadigitalservices@gmail.com>
2025-08-01 15:36:27 -0700

    Commented strict mode in main.jsx to avoid double  game over alerts in GameBoard.jsx

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  // <StrictMode> // this is commented to avoid double game over alerts in GameBoard.jsx
     <App />
-  </StrictMode>,
+ ///* </StrictMode>, */},
 )
